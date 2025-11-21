c06857a9068a1a7ead83e6758502f7acc32f9cc4
valluvan-dev <btreerepo.pythonfsd@gmail.com>
2025-11-21 16:59:47 +0530

    Add library management system with abstract class and methods for book operations

--- oops_bat.py
+++ oops_bat.py
@@ -143,6 +143,7 @@ from abc import ABC,abstractmethod
 
 
 
+"""
 class myclass(ABC):
 
     @abstractmethod
@@ -160,3 +161,207 @@ class child(myclass):
 p1 = child()
 
 p1.myfun()
+
+"""
+
+# print("Wecome")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  Python Project :
+
+
+
+class Library(ABC):
+
+
+    @abstractmethod
+    def add_book(self):
+        pass
+
+    @abstractmethod
+    def view_book(self):
+        pass
+
+    @abstractmethod
+    def issue_book(self):
+        pass
+
+    @abstractmethod
+    def return_book(self):
+        pass
+
+    @abstractmethod
+    def delete_book(self):
+        pass
+
+
+
+class College(Library):
+    
+    def __init__(self,x):
+        
+        self.storage = x
+
+    def add_book(self):
+        
+        if self.storage == []:
+            
+            count = int(input("How many books do you want to add? "))
+            
+            for i in range(count):
+                
+                book_name = input("Enter Book Name : ")
+                
+                self.storage.append(book_name)
+                
+            print("Books Added Successfully..")
+            
+        else:
+            
+            book_name = input("Enter Book Name : ")
+            
+            self.storage.append(book_name)
+            
+            print("Book Added Sucessfully")
+            
+
+    def view_book(self):
+        
+        for sno,name in enumerate(self.storage,101):
+            
+            print(f"{sno} - {name}")
+
+    def issue_book(self):
+        
+        issue_book = input("Enter the book name that you want? ")
+
+        if issue_book in self.storage:
+            
+            self.storage.remove(issue_book)
+            
+            print("Book issued Successfully")
+            
+        else:
+            
+            print("Book is not Available..!")
+        
+    def return_book(self):
+        
+        return_book = input("Enter the book name you rturned : ")
+        
+        self.storage.append(return_book)
+        
+
+    def delete_book(self):
+        
+        delete_book = input("Which book do you want to delete? ")
+
+
+        if delete_book in self.storage:
+            
+            self.storage.remove(delete_book)
+            
+            print("Book Removed Successfully")
+            
+        else:
+            
+            print("Book is Not At the Store")
+
+store_books = []
+
+obj = College(store_books)
+
+
+while True:
+
+    print("\nWelcome..!")
+
+    print("\nChoose your Option : ")
+
+    print("\n1. Add book")
+    print("\n2. View book")
+    print("\n3. Issue book")
+    print("\n4. Return book")
+    print("\n5. Delete book")
+    print("\n6. Exit")
+    
+    
+    option = int(input("\nEnter your Option as a Number : 1/2/3/4/5/6"))
+
+
+    if option == 1:
+        
+        obj.add_book()
+        
+    elif option == 2:
+        
+        obj.view_book()
+        
+    elif option == 3:
+        
+        obj.issue_book()
+        
+    elif option == 4:
+        
+        obj.return_book()
+        
+    elif option == 5:
+        
+        obj.delete_book()
+        
+    else:
+        
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
